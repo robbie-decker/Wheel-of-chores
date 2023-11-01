a5ce19ffd1ca85de25edeced6c84682536068b21
Robbie Decker <robbd56@gmail.com>
2023-11-01 17:38:31 -0400

    Create model and schema for backend

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,10 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 
+var uri = "mongodb+srv://robbd56:Ghoulishhule.\"75@cluster0.6dfwv8y.mongodb.net/?retryWrites=true&w=majority"
+
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/your-database-name', {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -17,11 +19,26 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
+  console.log(Wheel.find());
 });
 
 // Define routes and APIs
 // Example:
 // app.use('/api', require('./routes/api'));
+
+// Course Modal Schema 
+const wheelSchema = new mongoose.Schema({ 
+  _id: Number, 
+  name: String, 
+  total: Number 
+}); 
+  
+const Wheel = mongoose.model('wheel', wheelSchema);
+
+app.get('/', (req, res) => {
+  res.send('Hi World!')
+})
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
